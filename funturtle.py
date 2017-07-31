a1c624d1bc8cc66fa70380c270cf55a7816aacf2
rawand19-meet <rawand19@meet.mit.edu>
2017-07-31 18:30:06 +0300

    Stopping here

--- funturtle.py
+++ funturtle.py
@@ -1,4 +1,30 @@
-up_arrow="up"
-left_arrow="left"
-down_arrow"down"
-right
+UP_ARROW="up"
+LEFT_ARROW="left"
+DOWN_ARROW="down"
+RIGHT_ARROW="right" b
+SPACEBAR="space"
+UP=0
+DOWN=2
+LEFT=1
+RIGHT=3
+
+
+
+direction=UP
+def up ():
+    global direction
+    direction=UP
+    print('you pressed up')
+def down():
+    global direction
+    direction=DOWN
+
+def left():
+    global direction
+    direction=LEFT
+    
+def right():
+    global direction
+    direction=RIGHT
+    
+print(UP)
